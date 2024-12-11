615f3e8c5ca9e5bdfad93bc564d4496afc007d2b
adam <adampink99@gmail.com>
2024-12-11 14:34:38 +0000

    fixed

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,10 +9,6 @@ import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
-
-    <html lang="en">
-      
-
     <ClerkProvider>
       <html lang="en">
         <body>
