d717b421cc61f24b8270c630f67aabf3d7a354a8
Nicholas-Fearon <nejfearon@gmail.com>
2024-12-11 14:23:19 +0000

    clerk installed

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,26 @@
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+import "./globals.css";
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
