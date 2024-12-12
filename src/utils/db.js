f9ade27bf822f12c1b38d5e2079b327629d6cdea
adam <adampink99@gmail.com>
2024-12-12 14:59:36 +0000

    lookatallthedata

--- src/utils/db.js
+++ src/utils/db.js
@@ -2,4 +2,4 @@ import pg from "pg";
 
 export const db = new pg.Pool({ connectionString: process.env.DATABASE_URL });
 
-export const key = { connectionString: process.env.API_KEY };
+export const key = new pg.Pool({ connectionString: process.env.API_KEY });
