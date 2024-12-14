d535d1fc12b76767d53c60003a018c13f74531dc
Stefan-smiles <s.cookesec@gmail.com>
2024-12-14 13:28:17 +0000

    democracy

--- src/components/ReviewForm.jsx
+++ src/components/ReviewForm.jsx
@@ -1,34 +1,8 @@
-import * as Form from '@radix-ui/react-form';
-import React from 'react';
-
 export default function ReviewForm() {
-    const handleSubmit = (event) => {
-        event.preventDefault();
-    const formData = new FormData(event.target);
-    const data = Object.fromEntries(formData.entries());
-    console.log('Form Data:', data);
-  };
-
 return(
-    <Form.Root>
-    <Form.Field name="media-title">
-        <Form.Label>media-title</Form.Label>
-        <Form.Message>please enter a media-title</Form.Message>
-        <Form.ValidityState />
-    </Form.Field>
-
-    <Form.Field name="review">
-        <Form.Label />
-        <Form.Control />
-        <Form.Message />
-        <Form.ValidityState />
-    </Form.Field>
-
-    <Form.Message />
-    <Form.ValidityState />
-
-    <Form.Submit />
-</Form.Root>
-);
-
-);}
+    //input for media title
+    //input for reveiw
+    //
+    
+)
+}
