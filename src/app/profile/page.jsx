61307b4ef4403911c6b16f5d33a82d5a1326d470
Stefan-smiles <s.cookesec@gmail.com>
2024-12-16 15:14:02 +0000

    user page stuff added

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,11 +1,18 @@
+import { db } from "@/utils/db";
 import UserForm from "@/components/UserForm";
-import { currentUser } from "@clerk/nextjs/server";
+import { currentUser, auth } from "@clerk/nextjs/server";
 import { auth } from "@clerk/nextjs/server";
 import Link from "next/link";
 
 export default async function ProfileForm() {
   const { userId } = await auth();
   const user = await currentUser();
+  
+  const responseUser= await db.query 
+  ('SELECT bio FROM users WHERE clerk_id= `${userId}`');
+  const currentUserData= responseUser?.rows[0];
+
+  
   console.log("This is my current user:", user);
 console.log("This is my userId:",userId)
   if (!userId) {
@@ -15,17 +22,14 @@ console.log("This is my userId:",userId)
         <Link href="/sign-in">Sign In</Link>
       </div>
     );
-  }else{
+  }else {
+    if{(!bio)
     return (<>
           <h1>Welcome {user.firstName}</h1>
           <p>To leave reviews on films and Tv shows, fill in the form below:</p>
-          <UserForm />
-      </>)
-  }
-
- 
-
-
-
-  
+          <UserForm /></>}
+           else { return (<>
+            <h1>Welcome {user.firstName}</h1><p> {currentUser} </p></> }
+      
+  }  
 }
