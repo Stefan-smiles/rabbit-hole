08c397988183df9c9f66d7b9ff751a0c0f974544
adam <adampink99@gmail.com>
2024-12-17 11:06:00 +0000

    searchysearch

--- src/app/films/page.jsx
+++ src/app/films/page.jsx
@@ -1,10 +1,9 @@
-import { MovieSearch } from "@/components/MovieSearch";
 import Film from "@/components/film";
+import MovieSearch from "@/components/MovieSearch";
 export default function Films() {
   return (
     <div>
       <h2>welcome to the film page</h2>
-
       <MovieSearch />
     </div>
   );
--- src/components/MovieSearch.jsx
+++ src/components/MovieSearch.jsx
@@ -1,64 +1,32 @@
 "use client";
 import { useState } from "react";
-import Film from "./film";
-import Image from "next/image";
-
-<Film />;
 
 export default function MovieSearch() {
-  // State to manage the search term input by the user
+  const [formInputs, setForminputs] = useState({});
   const [searchTerm, setSearchTerm] = useState("");
-  // State to manage the movie details retrieved from the API
-  const [movieDetails, setMovieDetails] = useState(null);
-
-  const handleSearch = async () => {
-    setMovieDetail(null);
-    try {
-      const response = await fetch(
-        `https://api.themoviedb.org/3/search/movie?query=${searchTerm}api_key=d7186b2a1c0ff8a8bc39d8b6ff75b39b&page`
-      );
-      console.log(response);
-
-      const data = await response.json();
-
-      console.log(data);
-      setMovieDetails = data;
-    } finally {
-    }
-    const handleChange = (e) => {
-      setSearchTerm(e.target.value);
-    };
-    return (
-      <div>
-        <h2>Rabbit-hole </h2>
-        <div>
-          <input
-            type="text"
-            placeholder="enter movie title"
-            value={searchTerm}
-            oninput={handleChange}
-          />
-          <button onclick={handleSearch}>Search</button>
-        </div>
-
-        {movieDetails && (
-          <div>
-            <div>
-              <Image
-                src="https://image.tmdb.org/t/p/w500${movieDetails.poster_path}"
-                alt={movieDetails.title}
-                width="200"
-                height="300"
-              />
-            </div>
-
-            <div>
-              <h2>${movieDetails.title}</h2>
-              <p>${movieDetails.overview}</p>
-            </div>
-          </div>
-        )}
-      </div>
-    );
+  const handleInput = (event) => {
+    let { name, value } = event.target;
+    setForminputs({ ...formInputs, [name]: value });
+    setSearchTerm(event.target.value);
   };
+  const search = async (event) => {
+    event.preventDefault();
+    let movies = await fetch(
+      `https://api.themoviedb.org/3/search/movie?api_key=d7186b2a1c0ff8a8bc39d8b6ff75b39bquery=${forminputs.searchTerm}&page-1&include_adult=false`
+    );
+    movies = await movies.json();
+    setSearchResults(movies.results);
+  };
+  return (
+    <form onSubmit={search}>
+      <input
+        className="search"
+        name="searchTerm"
+        value={searchTerm}
+        onChange={handleInput}
+        type="text"
+      />
+      <button className="btn-search">search</button>
+    </form>
+  );
 }
