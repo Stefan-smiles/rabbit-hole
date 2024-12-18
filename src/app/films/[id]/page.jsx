4ef60cbbb7a6ccb49c195c9eacd3c025cb266e94
adam <adampink99@gmail.com>
2024-12-18 21:46:48 +0000

    tireddude

--- src/app/films/[id]/page.jsx
+++ src/app/films/[id]/page.jsx
@@ -1,13 +1,17 @@
 import ReviewForm from "@/components/ReviewForm";
 import Image from "next/image";
-
+import { auth } from "@clerk/nextjs/server";
+import { db } from "@/utils/db";
 export default async function MovieDetails({ params }) {
+  const { userId } = await auth();
   const { id } = await params; // Dynamic route params
   const response = await fetch(
     `https://api.themoviedb.org/3/movie/${id}?api_key=d7186b2a1c0ff8a8bc39d8b6ff75b39b`
   );
   const movie = await response.json();
-
+  const reviews = await db.query(`SELECT * FROM review where movie_id = ${id}`);
+  const content = await reviews.rows[0];
+  console.log(content);
   return (
     <main className="bg-gradient-to-b from-gray-900 to-gray-800 text-white min-h-screen p-8">
       <h1 className="text-4xl font-extrabold mb-8 text-center tracking-wider">
@@ -28,6 +32,11 @@ export default async function MovieDetails({ params }) {
             Release Date: {movie.release_date}
           </p>
           <ReviewForm movieid={id} />
+          {/* return(
+          <div>
+            <p>{content[0].content}</p>
+          </div>
+          ) */}
         </div>
       </div>
     </main>
